b9dad10050441be93bc63199dc9091feb12a390b
twoo1999 <twoo19999@gmail.com>
2024-11-26 23:06:37 +0900

    2024-11-26 프로그래머스  LEVEL3 양과 늑대

--- Programmers/LEVEL3/LEVEL3_양과 늑대.java
+++ Programmers/LEVEL3/LEVEL3_양과 늑대.java
@@ -5,11 +5,13 @@ class Solution {
         Node right;
         int num;
         int animal;
-        Set<Integer> hidewolf;
-        int depth;
+        int s;
+        int w;
+        int cnt;
         public Node(int animal){
             this.animal = animal;
-            this.hidewolf = new HashSet<>();
+            this.s = 0;
+            this.w = 0;
         }
 
     }
@@ -33,76 +35,219 @@ class Solution {
             } else{
                 nodes[parent].right= nodes[child];
             }
-            nodes[child].depth = nodes[parent].depth+1;
             nodes[child].num = child;
 
         }
 
-        int len = info.length;
-        int cnt = 0;
+        int[] count = new int[info.length];
+        Arrays.fill(count, Integer.MAX_VALUE);
+        for (int i = 0; i < info.length; i++) {
+            if(nodes[i].left != null) dfs(count, nodes[i].left, i, 1);
+            if(nodes[i].right != null) dfs(count, nodes[i].right, i, 1);
+            nodes[i].cnt = count[i];
 
-        int sheep = 0;
-//        int wolf = 0;
-        Set<Integer> wolf = new HashSet<>();
-//        Queue<Node> q = new LinkedList<>();
-        PriorityQueue<Node> q = new PriorityQueue<>((x, y)->{
-            if (x.hidewolf.size() != y.hidewolf.size()) {
-                return  x.hidewolf.size()-y.hidewolf.size();
-            } else{
-                return x.depth - y.depth;
-            }
-
-        });
-        q.add(nodes[0]);
-        Set<Integer> s = new HashSet<>();
-        while (!q.isEmpty()) {
-            Node curr = q.poll();
-
-            if(curr == nodes[0]){
-                sheep++;
-                if(curr.left != null) q.add(curr.left);
-                if(curr.right != null) q.add(curr.right);
-                continue;
-            }
-
-            if (curr.animal == 0) {
-
-                if(getSize(curr.hidewolf, wolf) >= sheep){ // 늑대가 많은 경우
-                    q.add(curr);
-                    int size = s.size();
-                    s.add(curr.num);
-                    if(s.size() == size){
-                        break;
-                    }
-                } else { // 양이 더 많은 경우
-                    s.clear();
-                    sheep++;
-                    wolf.addAll(curr.hidewolf);
-                    if(curr.left != null) q.add(curr.left);
-                    if(curr.right != null) q.add(curr.right);
-                }
-            } else {
-                if(curr.left != null){
-                    curr.left.hidewolf.add(curr.num);
-                    curr.left.hidewolf.addAll(curr.hidewolf);
-                    q.add(curr.left);
-                }
-                if(curr.right != null){
-                    curr.right.hidewolf.add(curr.num);
-                    curr.right.hidewolf.addAll(curr.hidewolf);
-                    q.add(curr.right);
-                }
-            }
+            if(nodes[i].left == null && nodes[i].right == null && nodes[i].animal == 0) count[i] = 0;
         }
 
-        answer = sheep;
+
+//        Queue<Integer> q = new LinkedList<>();
+//        q.add(0);
+//        HashSet<Integer> sheep = new HashSet<>();
+//        HashSet<Integer> wolf = new HashSet<>();
+        int[] a = {0, 0};
+        int[] b = {0, 0};
+        if(nodes[0].left != null) a = dfs1(nodes[0].left);
+        if(nodes[0].right != null) b = dfs1(nodes[0].right);
+
+        nodes[0].s = a[0] + b[0];
+        nodes[0].w = a[1] + b[1];
+
+        PriorityQueue<Node> pq = new PriorityQueue<>((x, y)->{
+
+
+            int xx = x.s - x.w;
+            int yy = y.s - y.w;
+
+            if (xx != yy) {
+                return yy - xx;
+            }
+            if (x.cnt != y.cnt) {
+                return x.cnt - y.cnt;
+            }
+
+            return y.s - x.s;
+        });
+
+
+//        PriorityQueue<Node> pq = new PriorityQueue<>((x, y)->{
+//            return x.w - y.w;
+////            int yy = y.s - y.w;
+////            int xx = x.s - x.w;
+////
+////            if (yy != xx) {
+////                return (y.s - y.w) - (x.s - x.w);
+////            } else{
+////                return y.s - x.s;
+////            }
+//
+//        });
+////        Queue<Node> pq = new LinkedList<>();
+        pq.add(nodes[0]);
+        ArrayList<Node> ar = new ArrayList<>();
+        int s = 0;
+        int w = 0;
+        while (!pq.isEmpty()) {
+            Node curr = pq.poll();
+
+            int ww = w;
+            int ss = s;
+
+
+            if(curr.animal == 1) ww++;
+            else ss++;
+
+            if (ss > ww) {
+                s = ss;
+                w = ww;
+                if(curr.left != null) pq.add(curr.left);
+                if(curr.right != null) pq.add(curr.right);
+                for (int i = 0; i < ar.size(); i++) {
+                    pq.add(ar.get(i));
+                }
+                ar.clear();
+            } else{
+                ar.add(curr);
+            }
+
+        }
+//
+//
+//        int w = 0;
+//        int s = 0;
+//
+//        ArrayList<Node> ar = new ArrayList<>();
+//        while (!pq.isEmpty()) {
+////        while(cnt < 50){
+//            Node curr = pq.poll();
+//            int animal = curr.animal;
+//
+//
+//            int ww = w;
+//            int ss = s;
+//
+//
+//            if(curr.s == 0) continue;
+//            if(animal == 1) ww++;
+//            else ss++;
+//
+//            if (ss > ww) {
+//                s = ss;
+//                w = ww;
+//                if(curr.left != null) pq.add(curr.left);
+//                if(curr.right != null) pq.add(curr.right);
+//                for (int i = 0; i < ar.size(); i++) {
+//                    pq.add(ar.get(i));
+//                }
+//                ar.clear();
+//            } else{
+//                ar.add(curr);
+//            }
+//
+//
+//
+//        }
+//        answer = s;
+////        int prev = -1;
+////        int size = 0;
+////        while (prev != size) {
+////            dfs(nodes[0].left, sheep, wolf);
+////            dfs(nodes[0].right, sheep, wolf);
+////            size = sheep.size();
+////            prev = size;
+////        }
+////
+////
+////        System.out.println(prev);
+////        System.out.println(size);
+
+        answer = s;
 
 
         return answer;
     }
-    static int getSize(Set<Integer> s1, Set<Integer> s2) {
-        Set<Integer> s = new HashSet<>(s1);
-        s.addAll(s2);
-        return s.size();
+
+    //    static int[] dfs(Node node) {
+//        int s = 0;
+//        int w = 0;
+//        int animal = node.animal;
+//        if(animal == 1) w++;
+//        else s++;
+//
+//        if(node.right == null && node.left == null){
+//            node.w = w;
+//            node.s = s;
+//
+//            return new int[]{s, w};
+//        }
+//
+//
+//
+//
+//        if(node.left != null) {
+//            int[] r = dfs(node.left);
+//            s += r[0];
+//            w += r[1];
+//        }
+//        if(node.right != null) {
+//            int[] r = dfs(node.right);
+//            s += r[0];
+//            w += r[1];
+//        }
+//
+//        node.s = s;
+//        node.w = w;
+//
+//        return new int[]{s, w};
+//    }
+    static int[] dfs1(Node node) {
+        int s = 0;
+        int w = 0;
+        int animal = node.animal;
+        if(animal == 1) w++;
+        else s++;
+
+        if(node.right == null && node.left == null){
+            node.w = w;
+            node.s = s;
+
+            return new int[]{s, w};
+        }
+
+
+
+
+        if(node.left != null) {
+            int[] r = dfs1(node.left);
+            s += r[0];
+            w += r[1];
+        }
+        if(node.right != null) {
+            int[] r = dfs1(node.right);
+            s += r[0];
+            w += r[1];
+        }
+
+        node.s = s;
+        node.w = w;
+
+        return new int[]{s, w};
+    }
+    static void dfs(int[] count, Node node, int n, int cnt) {
+        if (node.animal == 0) {
+            count[n] = Math.min(cnt, count[n]);
+        }
+
+        if(node.right != null) dfs(count, node.right, n, cnt+1);
+        if(node.left != null) dfs(count, node.left, n, cnt+1);
     }
 }
